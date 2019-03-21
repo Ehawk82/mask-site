21b4df8cafa38d7d6e9ca5b12578779972abc30a
Jason Graziano <mrehawk82@gmail.com>
2019-03-21 09:11:07 -0400

    doing stuff

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,8 @@ var myUI = {
 		    mainArea = createEle("div");
         
         navBar.className = "navBar";
-        myUI.generateNavItems(navBar);
         
         mainArea.className = "mainArea";
-        myUI.generateMainItems(mainArea);
 
         header.innerHTML = "THE SITE";
         
@@ -25,45 +23,6 @@ var myUI = {
         dH.append(footer);
 
 		dvContain.append(dH); 
-	},
-	generateNavItems: (navBar) => {
-        var navToggleBtn = createEle("button");
-        
-        navToggleBtn.innerHTML = "&#9776;";
-        navToggleBtn.className = "navToggleBtn";
-        navToggleBtn.onclick = myUI.navToggleFunc(navToggleBtn, navBar);
-
-        navBar.append(navToggleBtn);
-	},
-	generateMainItems: (mainArea) => {
-		var mainAreaStuff = createEle("div"),
-		    mainAreaStuff2 = createEle("div"),
-		    mainAreaStuff3 = createEle("div");
-
-        mainAreaStuff3.className = "mainAreaStuff3";
-		mainAreaStuff3.innerHTML = "&nbsp;";
-
-		mainAreaStuff2.className = "mainAreaStuff2";
-		mainAreaStuff2.innerHTML = "This is the area for secondary stuffs";
-
-		mainAreaStuff.className = "mainAreaStuff";
-		mainAreaStuff.innerHTML = "This is the area for primary stuffs";
-
-		mainArea.append(mainAreaStuff);
-		mainArea.append(mainAreaStuff2);
-		mainArea.append(mainAreaStuff3);
-
-	},
-	navToggleFunc: (navToggleBtn, navBar) => {
-		return () => {
-			if (navBar.className === "navBar") {
-				makeFull(navBar);
-				makeFull(navToggleBtn);
-			}else if (navBar.className === "navBar_full") {
-				takeFull(navBar);
-				takeFull(navToggleBtn);
-			}
-		}
 	}
 };
 window.onload = () => {
