7780df5ed675d46feed15b57822be8e2c3adefb1
Jason Graziano <mrehawk82@gmail.com>
2019-03-23 12:41:27 -0400

    catching up with myself

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,18 @@ var myUI = {
 		    header = createEle("header"),
 		    section = createEle("section"),
 		    footer = createEle("footer"),
-		    navBar = createEle("div"),
+		    navBar = createEle("nav"),
 		    mainArea = createEle("div");
         
         navBar.className = "navBar";
-        myUI.generateNavItems(navBar);
         
         mainArea.className = "mainArea";
+
+        header.innerHTML = "HOLO-EX";
+
         myUI.generateMainItems(mainArea);
 
-        header.innerHTML = "THE SITE";
+        myUI.generateNavItems(navBar, mainArea);
         
         section.append(navBar);
         section.append(mainArea);
@@ -26,13 +28,14 @@ var myUI = {
 
 		dvContain.append(dH); 
 	},
-	generateNavItems: (navBar) => {
+	generateNavItems: (navBar, mainArea) => {
 		for (var i = 0; i < 5; i++) {
-			var btns = createEle("button");
+			var btns = createEle("button"),
+			    ele = navText[i].toLowerCase() + "PageBtn";
 			btns.className = "navBtns";
-			btns.id = "navBtn_" + i;
+			btns.id = ele;
 			btns.innerHTML = navText[i];
-			btns.onclick = myUI.doNavFunction(btns, i);
+			btns.onclick = myUI.doNavFunction(btns, i, mainArea);
 
             navBar.append(btns);
 		}
@@ -41,19 +44,49 @@ var myUI = {
 		var item1 = createEle("div");
 
 		item1.className = "mainItems";
-		item1.innerHTML = homePage[0];
+		item1.innerHTML = homePage[0] + homePage[1] + homePage[2] + homePage[3] + homePage[4] + homePage[5];
 
         mainArea.append(item1);
+        setTimeout(() => {
+			    makeFull(mainArea);
+
+			}, 200);
 	},
-	doNavFunction: (btns, i) => {
+	doNavFunction: (btns, i, mainArea) => {
 		return () => {
-            console.log(btns.innerHTML);
+
+			takeFull(mainArea);
+		
+			var mainItems = bySel(".mainItems"), x;
+
+			if (i === 0) {
+				x = homePage;
+			}
+            if (i === 1) {
+				x = shopPage;
+			}
+			if (i === 2) {
+				x = designPage;
+			}
+			if (i === 3) {
+				x = contactPage;
+			}
+			if (i === 4) {
+				x = aboutPage;
+			}
+			setTimeout(() => {
+			mainItems.innerHTML = x[0] + x[1] + x[2] + x[3] + x[4] + x[5];
+			}, 530);
+			setTimeout(() => {
+				if (mainArea.className === "mainArea_full") {
+                /*err check on time for className change*/
+				} else {
+                    makeFull(mainArea);
+				}
+			}, 500);
 	    }
     }
 };
 window.onload = () => {
 	myUI.init();
-};
-window.onresize = () => {
-	location.reload();
 };
